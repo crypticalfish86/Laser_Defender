647a11282b5b97337f325cfcbee6a1e3a63d55ca
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-08-10 01:24:33 +0100

    added player screen boundaries

--- Assets/Scripts/PlayerShipScript.cs
+++ Assets/Scripts/PlayerShipScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,22 +10,48 @@ public class PlayerShipScript : MonoBehaviour
     //player movement
     [SerializeField] private float movementSpeed; //movement speed multiplier
     private Vector2 rawMoveInputVector; //raw movement input
-        private Vector3 adjustedMovementVector; //movement adjusted for frame rate + movement speed
+        private Vector3 upcomingMovementVector; //movement adjusted for frame rate + movement speed
     
         //get the raw movement input value from the player input component
         private void OnMove(InputValue value) {
             rawMoveInputVector = value.Get<Vector2>();
         }
 
+    //player boundaries
+        [SerializeField] float boundaryPaddingX;
+        [SerializeField] float boundaryPaddingY;
+        Vector2 minimumPlayerMapBoundary;
+        Vector2 maximumPlayerMapBoundary;
+
+        //Initialize player boundaries using main camera viewport values
+        private void InitiatePlayerBoundaryValues() {
+            Camera mainCamera = Camera.main;
+            minimumPlayerMapBoundary = mainCamera.ViewportToWorldPoint(new Vector2 (0,0));
+            maximumPlayerMapBoundary = mainCamera.ViewportToWorldPoint(new Vector2 (1,1));
+        }
+
+    private void Start() {
+        InitiatePlayerBoundaryValues();
+    }
 
     // Update is called once per frame
     private void Update() {
         PlayerMovement();
     }
 
-    //adjust movement by frame rate and movementspeed multiplier and then move player
+    /*
+        executes player movement each frame:
+        1.multiplies the raw input vector by framerate and movement speed multiplier and applies it to upcoming movement
+        2.ensures the upcoming movement doesn't take the upcoming position outside of the boundaries of the viewport space using Math.Clamp(value, minValue, maxValue)
+        3.applies that new position to the player
+    */
     private void PlayerMovement() {
-        adjustedMovementVector = rawMoveInputVector * movementSpeed * Time.deltaTime;
-        transform.position += adjustedMovementVector;
+        upcomingMovementVector = rawMoveInputVector * movementSpeed * Time.deltaTime;
+        
+        Vector2 upComingPosition = new Vector2();
+        upComingPosition.x = Math.Clamp(transform.position.x + upcomingMovementVector.x, minimumPlayerMapBoundary.x + boundaryPaddingX, maximumPlayerMapBoundary.x - boundaryPaddingX);
+        upComingPosition.y = Math.Clamp(transform.position.y + upcomingMovementVector.y, minimumPlayerMapBoundary.y + boundaryPaddingY, maximumPlayerMapBoundary.y - boundaryPaddingY);
+        
+        transform.position = upComingPosition;
     }
 }
